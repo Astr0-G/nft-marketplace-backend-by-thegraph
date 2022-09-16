fe2f54353a07291abdecee929e50eda08f608065
wg326 <57165451+wg326@users.noreply.github.com>
2022-09-16 16:12:40 -0700

    first commit

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Header() {
     return (
         <nav className="non-italic font-bold bg-liyellow text-yinblue flex flex-row justify-between items-center">
-            <h1 className="py-4 px-4 font-bold text-3xl">Polygon NFT Marketplace</h1>
+            <h1 className="py-4 px-4 font-bold text-3xl">Goerli NFT Marketplace</h1>
             <div className="flex flex-row items-center">
                 <Link href="/">
                     <button className="mr-4 p-6">HOME</button>
